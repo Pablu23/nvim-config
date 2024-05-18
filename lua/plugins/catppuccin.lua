d8ad94bbe223da4f798759c0a9cc3af69356ae70
Pablu23 <pablu23@gmx.de>
2024-05-18 16:40:57 +0200

    Neotree toggle, catppuccin background transparent

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,6 +4,10 @@ return {
 	priority = 1000,
 	lazy = false,
 	config= function()
+    require("catppuccin").setup({
+      transparent_background = true,
+    })
+
 		vim.cmd.colorscheme "catppuccin"
 	end
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,3 +5,8 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set expandtab")
 vim.cmd("set number")
 vim.cmd("set clipboard+=unnamedplus")
+vim.cmd("highlight Normal guibg=none")
+vim.cmd("highlight NonText guibg=none")
+vim.cmd("highlight Normal ctermbg=none")
+vim.cmd("highlight NonText ctermbg=none")
+

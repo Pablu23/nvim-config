1cda3098b29b60b9a447422eb3266d83397f3373
Pablu23 <pablu23@gmx.de>
2024-05-21 16:08:19 +0200

    Added html

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -13,6 +13,7 @@ return {
 					"lua_ls",
 					"gopls",
 					"rust_analyzer",
+          "html",
 				},
 			})
 		end,
@@ -32,9 +33,13 @@ return {
 			lspconfig.rust_analyzer.setup({
 				capabilities = capabilities,
 			})
+      lspconfig.html.setup({
+				capabilities = capabilities,
+      })
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {})
 		end,
 	},
 }

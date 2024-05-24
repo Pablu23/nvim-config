f0e62e14eebd0b3de4a0593cbd2c826e5a4f55b8
Pablu23 <pablu23@gmx.de>
2024-05-24 13:41:58 +0200

    Changed disabled transparency

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,9 +4,9 @@ return {
 	priority = 1000,
 	lazy = false,
 	config= function()
-    require("catppuccin").setup({
-      transparent_background = true,
-    })
+    -- require("catppuccin").setup({
+    --   transparent_background = true,
+    -- })
 
 		vim.cmd.colorscheme "catppuccin"
 	end
